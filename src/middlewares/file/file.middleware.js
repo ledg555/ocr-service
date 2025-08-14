f1673d820ad87ca366e62c9192524a9ae35bd5b8
Luis Delgado <dev.ledg555@gmail.com>
2025-08-13 19:42:21 -0600

    feat: change multer storage from memory to disk

--- src/middlewares/file/file.middleware.js
+++ src/middlewares/file/file.middleware.js
@@ -1,6 +1,14 @@
 import multer from "multer";
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "/uploads");
+  },
+  filename: function (req, file, cb) {
+    const prefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, prefix + "-" + file.originalname);
+  },
+});
 const upload = multer({ storage });
 const fileMiddleware = upload.single("file");
 
