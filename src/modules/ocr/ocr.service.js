25d57752e0125bbe970fd04e4376300135a4a825
ledg555 <dev.ledg555@gmail.com>
2025-08-14 01:22:29 -0600

    feat: pass disk path instead of file to extractText fn

--- src/modules/ocr/ocr.service.js
+++ src/modules/ocr/ocr.service.js
@@ -5,16 +5,10 @@ export class OCRService {
   }
 
   async extractText(multerFile) {
-    const buffer = multerFile.buffer;
-    const blob = new Blob([buffer], { type: multerFile.mimetype });
-    const file = new File([blob], multerFile.originalname, {
-      type: multerFile.mimetype,
-    });
-    const fileWithFileData = { ...file, fileData: buffer };
-
+    console.log({ multerFile });
     const text = await this.#ocrClient.extractText(
-      [fileWithFileData],
-      ["es", "esp", "sp", "spa", "spanish"]
+      [`./uploads/${multerFile.filename}`]
+      // ["es", "esp", "sp", "spa", "spanish"]
     );
     console.log("Extracted text:", text);
     return text;
